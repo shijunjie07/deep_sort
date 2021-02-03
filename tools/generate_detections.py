f6754b3da7ed264791befa3330555f73a1c54498
Michal Kucirka <michal.kucirka@cogniware.com>
2021-02-03 12:28:14 +0100

    generate_detection: code converted to Tensorflow 2 from version 1

--- tools/generate_detections.py
+++ tools/generate_detections.py
@@ -7,19 +7,18 @@ import cv2
 import tensorflow as tf
 
 
-def _run_in_batches(f, data_dict, out, batch_size):
+def _run_in_batches(f: callable, data: tf.Tensor, out: str, batch_size: int):
     data_len = len(out)
     num_batches = int(data_len / batch_size)
 
     s, e = 0, 0
     for i in range(num_batches):
         s, e = i * batch_size, (i + 1) * batch_size
-        batch_data_dict = {k: v[s:e] for k, v in data_dict.items()}
+        batch_data_dict = data[s:e]
         out[s:e] = f(batch_data_dict)
     if e < len(out):
-        batch_data_dict = {k: v[e:] for k, v in data_dict.items()}
+        batch_data_dict = data[e:]
         out[e:] = f(batch_data_dict)
-
 
 def extract_image_patch(image, bbox, patch_shape):
     """Extract image patch from bounding box.
@@ -68,30 +67,54 @@ def extract_image_patch(image, bbox, patch_shape):
     return image
 
 
-class ImageEncoder(object):
+def wrap_frozen_graph(graph_def, inputs, outputs):
+    ''' Helpe wrapper that imports tensorflow 1 graph into Tensorflow 2 object.
+    reference: https://www.tensorflow.org/guide/migrate
+    '''
+    def _imports_graph_def():
+        tf.compat.v1.import_graph_def(graph_def, name="")
+    wrapped_import = tf.compat.v1.wrap_function(_imports_graph_def, [])
+    import_graph = wrapped_import.graph
+    return wrapped_import.prune(
+        tf.nest.map_structure(import_graph.as_graph_element, inputs),
+        tf.nest.map_structure(import_graph.as_graph_element, outputs)
+    )
 
-    def __init__(self, checkpoint_filename, input_name="images",
-                 output_name="features"):
-        self.session = tf.Session()
-        with tf.gfile.GFile(checkpoint_filename, "rb") as file_handle:
-            graph_def = tf.GraphDef()
+class ImageEncoder(object):
+    def __init__(
+        self,
+        checkpoint_filename,
+        input_name="images",
+        output_name="features"
+    ):
+
+        with tf.io.gfile.GFile(checkpoint_filename, "rb") as file_handle:
+            graph_def = tf.compat.v1.GraphDef()
             graph_def.ParseFromString(file_handle.read())
         tf.import_graph_def(graph_def, name="net")
-        self.input_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % input_name)
-        self.output_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % output_name)
 
+        self.input_var = tf.compat.v1.get_default_graph().get_tensor_by_name(
+            f"{input_name}:0")
+        self.output_var = tf.compat.v1.get_default_graph().get_tensor_by_name(
+            f"{output_name}:0")
         assert len(self.output_var.get_shape()) == 2
         assert len(self.input_var.get_shape()) == 4
         self.feature_dim = self.output_var.get_shape().as_list()[-1]
         self.image_shape = self.input_var.get_shape().as_list()[1:]
 
+        self.graph_function = wrap_frozen_graph(
+            graph_def,
+            inputs=f'{input_name}:0',
+            outputs=f'{output_name}:0'
+        )
+
     def __call__(self, data_x, batch_size=32):
+        if isinstance(data_x, np.ndarray):
+            data_x = tf.convert_to_tensor(data_x)
         out = np.zeros((len(data_x), self.feature_dim), np.float32)
         _run_in_batches(
-            lambda x: self.session.run(self.output_var, feed_dict=x),
-            {self.input_var: data_x}, out, batch_size)
+            self.graph_function, data_x, out, batch_size
+        )
         return out
 
 
